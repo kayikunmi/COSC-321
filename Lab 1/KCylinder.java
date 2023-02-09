32098a89567d1664f205489ee51fe0624c1fe1ab
KayikunmiMac <kayikunmiba.com>
2023-02-09 10:02:03 -0500

    let's see

--- Lab 1/KCylinder.java
+++ Lab 1/KCylinder.java
@@ -1,13 +1,15 @@
 public class KCylinder extends Ri{
     void SolidCylinder(double radius, double zmin, double zmax){
 	RiCylinder(radius, zmin, zmax, 360.0, RI_NULL);
-	RiDisk(zmin, radius, 360.0, RI_NULL);
-	RiDisk(zmax, radius, 360.0, RI_NULL);
+	//RiSphere(zmin, radius, 360.0, RI_NULL);
+   // RiSphere(4.0, 1.0, 8.0, 360.0, RI_NULL);
+	//RiDisk(zmax, radius, 360.0, RI_NULL);
     }
 
     void object(){
         RiCylinder(6, 1, 12, 360, RI_NULL);
-        //RiTranslate(0.0, 0.0, -0.3);
+        //RiSphere(4.0, 1.0, 8.0, 360.0, RI_NULL);
+        RiTranslate(0.0, 2, 4);
     
     }
 }
