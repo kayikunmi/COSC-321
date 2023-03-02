2d5f297653f1bf28dc9f7a029e4d8a2ba4090337
KayikunmiMac <kayikunmiba.com>
2023-03-02 10:21:23 -0500

    all files

---
+++ Lab2/AC_CS321_Exception.java
@@ -0,0 +1,16 @@
+public 
+public class AC_CS321_Exception extends RuntimeException {
+
+	String name;
+	
+	public AC_CS321_Exception(String name) {
+		this.name = name;
+}
+
+	public String toString() {
+		return "RiShader called with shader "+name+" this is not supported in any way";
+	}
+}
+ AC_CS321_Exception {
+    
+}
