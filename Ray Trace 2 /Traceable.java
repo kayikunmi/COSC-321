72f340b781486a7ad4dd03fbe32c03769fe012cf
KayikunmiMac <kayikunmiba.com>
2023-03-23 14:27:04 -0400

    not sure if i did this correctly

--- Ray Trace 2 /Traceable.java
+++ Ray Trace 2 /Traceable.java
@@ -1,6 +1,5 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
+
 
 
 public abstract class Traceable {
@@ -12,22 +11,40 @@ public abstract class Traceable {
 		return this.local_intersect(r);
 	}; 
 
+	// public static Intersection hit(ArrayList<Intersection> result) {
+	// 	/* You need to write this - this should return 
+	// 	null if result is empty
+	// 	the Intersection with the smallest POSITIVE t value
+	// 	*/
+	// 	/*t is the amount youhave to multiply the direction by and add it to the origin
+	// 	 * greater than zero, but closest to 0
+	// 	 */
+	// 	Collections.sort(result);
+	// 	for (Intersection x:result){
+	// 		if(x.compareTo(x) >=0){
+	// 			//find the smallest
+	// 		}
+	// 	}
+	// }
+
 	public static Intersection hit(ArrayList<Intersection> result) {
-		/* You need to write this - this should return 
-		null if result is empty
-		the Intersection with the smallest POSITIVE t value
-		*/
-		/*t is the amount youhave to multiply the direction by and add it to the origin
-		 * greater than zero, but closest to 0
-		 */
-		Collections.sort(result);
-		for (Intersection x:result){
-			if(x.compareTo(x) >=0){
-				//find the smallest
+		if (result.isEmpty()) {
+			return null;
+		}
+		
+		Intersection minIntersection = null;
+		double minT = -1;  // Set to negative value to ensure any positive t value is smaller
+		
+		for (Intersection intersection : result) {
+			if (intersection.t > 0 && (minIntersection == null || intersection.t < minT)) {
+				minIntersection = intersection;
+				minT = intersection.t;
 			}
 		}
-
+		
+		return minIntersection;
 	}
+	
 
 
 	//merges two Intersection Lists  
