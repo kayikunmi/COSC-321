f8f692dd0059f7425c346c30906aa9bbf15fbade
KayikunmiMac <kayikunmiba.com>
2023-04-27 17:45:11 -0400

    car

--- Final/Final.java
+++ Final/Final.java
@@ -14,22 +14,10 @@ public class Final extends Ri {
     
         RiAttributeBegin();
         RiTranslate(-0.3,0.4,10.0-1.5);
-        RiScale(5.0,5.0,5.0);
+        RiScale(2,2.0,1);
         double[] darkOrange = {1.0,0.549,0};
         RiBxdf("PxrSurface","surface1", "int diffuseDoubleSided", 1, "color diffuseColor", darkOrange);
         new KObject().object();
-        RiAttributeEnd();
-
-        RiAttributeBegin();
-
-        double[] Color2 = {0.0,0.749,1.0};
-        RiBxdf("PxrSurface","surface1", "int diffuseDoubleSided", 1, "color diffuseColor",Color2);
-   
-        RiTranslate(2.0,3.0,5.0);
-        RiRotate(45.0+5.0,1.0,1.0,1.0);
-        RiScale(5.0,5.0,5.0);
-   
-        new JerObject2().object();
         RiAttributeEnd();
 
         RiAttributeBegin();
--- Final/KObject.java
+++ Final/KObject.java
@@ -7,28 +7,40 @@ public class KObject extends Ri{
     }
 
 
-    void object(){
-        SolidCylinder(0.1, -0.5, 0.5);
-        RiTranslate(0.0, 0.0, -0.3);
-        SolidCylinder(0.15, -0.05, 0.05);
-        RiTranslate(0.0, 0.0, 0.6);
-        SolidCylinder(0.15, -0.05, 0.05);
+    void object() {
+        // body
+        RiAttributeBegin();
+        double[] bodyColor = {1, 0.47, 0.2};
+        RiBxdf("PxrSurface", "bodySurface", "color diffuseColor", bodyColor);
         RiTransformBegin();
-        RiRotate(90.0, 1.0, 0.0, 0.0);
-        SolidCylinder(0.1, -0.5, 0.5);
-        RiTranslate(0.0, 0.0, -0.3);
-        SolidCylinder(0.15, -0.05, 0.05);
-        RiTranslate(0.0, 0.0, 0.6);
-        SolidCylinder(0.15, -0.05, 0.05);
+        RiScale(2.0, 1.0, 1.0);
+        RiSphere(1.0, -1.0, 1.0, 360.0, RI_NULL);
         RiTransformEnd();
         RiTransformBegin();
-        RiRotate(90.0, 0.0, 1.0, 0.0);
-        SolidCylinder(0.1, -0.5, 0.5);
-        RiTranslate(0.0, 0.0, -0.3);
-        SolidCylinder(0.15, -0.05, 0.05);
-        RiTranslate(0.0, 0.0, 0.6);
-        SolidCylinder(0.15, -0.05, 0.05);
+        RiScale(2.0, 1.0, 1.0);
+        RiTranslate(0.0, 0.0, 2.0);
+        RiSphere(1.0, -1.0, 1.0, 360.0, RI_NULL);
         RiTransformEnd();
-    }
+        RiTransformBegin();
+        RiTranslate(0.0, 0.5, 0.0);
+        RiScale(2.0, 0.5, 1.0);
+        RiCylinder(0.5, -1.0, 1.0, 360.0, RI_NULL);
+        RiTransformEnd();
+        RiAttributeEnd();
 
+        // wheels
+        double[] wheelColor = {0, 0, 0};
+        for (int i = -1; i <= 1; i += 2) {
+            for (int j = -1; j <= 1; j += 2) {
+                RiAttributeBegin();
+                RiTranslate(i, 0.0, j * 1.25);
+                RiBxdf("PxrSurface", "wheelSurface", "color diffuseColor", wheelColor);
+                RiTransformBegin();
+                RiScale(1.0, 1.0, 0.2);
+                RiCylinder(0.5, -1.0, 1.0, 360.0, RI_NULL);
+                RiTransformEnd();
+                RiAttributeEnd();
+            }
+        }
+    }
 }
